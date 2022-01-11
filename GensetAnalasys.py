152b7cb9a043fcdd6af0e25de59908c56c5e6dfc
oz182 <91877982+oz182@users.noreply.github.com>
2022-01-11 23:58:59 +0200

    Add files via upload

---
+++ GensetAnalasys.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import PySimpleGUI as sg
+import os
+
+
+
+
+def FirstLayout():
+    global FileNameIN
+
+    st.set_page_config(layout="wide")
+    st.write("""## **CSV analysis From - AMS** """)
+
+    my_expander = st.sidebar.expander(label='Files selection')
+    with my_expander:
+        FileNameIN = st.file_uploader('Choose a .csv file: ')
+
+
+
+def main():
+    FirstLayout()
+    print('nothing yet')
+
+
+if __name__ == "__main__":
+    main()
