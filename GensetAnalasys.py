0e00f7263494e7115c2f35e73f9587dac737eeb6
oz182 <91877982+oz182@users.noreply.github.com>
2022-01-12 11:43:20 +0200

    Add files via upload

--- GensetAnalasys.py
+++ GensetAnalasys.py
@@ -2,6 +2,34 @@ import streamlit as st
 import pandas as pd
 
 
+def DownSampleRate():
+    print('Nothing')
+
+
+def StreamLitGUI():
+    left_column, right_column = st.columns(2)
+
+    X = left_column.selectbox('', HaedersList)
+    left_column.line_chart(data=df[X])
+
+    Y = right_column.selectbox(' ', HaedersList)
+    right_column.line_chart(data=df[Y])
+
+    st.write("""# **The selected graphes combined** """)
+
+    stdf = pd.DataFrame(data, columns=[X, Y])
+    st.line_chart(stdf)
+
+
+
+def CSVfileAnalyser(fp):
+    global HaedersList, df, data
+
+    data = pd.read_csv(fp)
+    df = pd.DataFrame(data)
+    HaedersList = list(df.columns.values)
+
+    st.write(df)
 
 
 def FirstLayout():
@@ -15,11 +43,20 @@ def FirstLayout():
         FileNameIN = st.file_uploader('Choose a .csv file: ')
 
 
-
 def main():
     FirstLayout()
-    print('nothing yet')
+    CSVfileAnalyser(FileNameIN)
+    StreamLitGUI()
+
+
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+    if X != Y:
+        c = alt.Chart(stdf).mark_circle(size=60).encode(x=X, y=Y).interactive()
+        st.altair_chart(c, use_container_width=True)
+"""
