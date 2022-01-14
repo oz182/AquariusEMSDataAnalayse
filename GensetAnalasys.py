47e77196dcb3c0a98a9fbbe9452bd47e4f14da76
oz182 <91877982+oz182@users.noreply.github.com>
2022-01-14 14:02:58 +0200

    Add files via upload

--- GensetAnalasys.py
+++ GensetAnalasys.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
 
 def DownSampleRate():
     print('Nothing')
@@ -16,14 +17,22 @@ def StreamLitGUI():
     Y = right_column.selectbox(' ', HaedersList)
     right_column.line_chart(data=df[Y])
 
-    st.write("""# **The selected graphes combined** """)
-
-    stdf = pd.DataFrame(data, columns=[X, Y])
-    st.line_chart(stdf)
-
     if X != Y:
+        st.write("""# **The selected graphes combined** """)
+
+        stdf = pd.DataFrame(data, columns=[X, Y])
+        st.line_chart(stdf)
+
         c = alt.Chart(stdf).mark_circle(size=60).encode(x=X, y=Y).interactive()
         st.altair_chart(c, use_container_width=True)
+
+    #just for now... this how to do a graph about the time axis but it is not dynamic
+        #tempdf = pd.DataFrame(data, columns=['Time', X])
+        #lineA = alt.Chart(tempdf).mark_line().encode(x='Time', y=X).interactive()
+        #st.altair_chart(lineA)
+
+    #pr = df.profile_report()
+    #st_profile_report(pr)
 
 
 
@@ -32,7 +41,7 @@ def CSVfileAnalyser(fp):
 
     data = pd.read_csv(fp)
     df = pd.DataFrame(data)
-    HaedersList = list(df.columns.values)
+    HaedersList = sorted(list(df.columns.values))
 
 
 def FirstLayout():
